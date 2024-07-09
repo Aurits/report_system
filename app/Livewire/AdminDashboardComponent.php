db28b0fc2def50bdff34a1fda7b79ba6ee3d190d
Ambrose Alanda <alandaambrose@gmail.com>
2024-07-09 19:41:02 +0300

    send

---
+++ app/Livewire/AdminDashboardComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AdminDashboardComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin-dashboard-component');
+    }
+}
