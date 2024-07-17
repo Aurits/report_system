f9568050cf2931cb1d2067ce58284975c2b8c77c
Ambrose Alanda <alandaambrose@gmail.com>
2024-07-17 13:01:09 +0300

    send

--- resources/views/livewire/teachers-component.blade.php
+++ resources/views/livewire/teachers-component.blade.php
@@ -55,7 +55,7 @@
                             </div>
 
                             <div class="table-responsive">
-                                <table class="table border-0 star-teacher table-hover dataTable table-center mb-0 table-striped">
+                                <table class="table border-0 star-teacher table-hover dataTables table-center mb-0 table-striped">
                                     <thead class="teacher-thread">
                                         <tr>
                                             <th>ID</th>
